37501ff30a4f308b2c874fe20dc9deeb00cf73d6
Diogo Machado <diogo@diogomachado.com>
2024-05-22 13:48:57 -0300

    fix: added topics

---
+++ data/topics.js
@@ -0,0 +1,87 @@
+const topics = [
+  {
+    title: "Introdução",
+    sections: [
+      {
+        name: "Como funciona o Tailwind",
+      },
+      {
+        name: "Quando usar um framework de forma estratégica para seu projeto",
+      },
+    ],
+  },
+  {
+    title: "Integrando com frameworks SPA famosos",
+    sections: [
+      {
+        name: "React",
+      },
+      {
+        name: "Angular",
+      },
+      {
+        name: "Vue.js",
+      },
+    ],
+  },
+  {
+    title: "Design responsivo",
+    sections: [
+      {
+        name: "Breapoints",
+      },
+    ],
+  },
+  {
+    title: "Templates",
+    sections: [
+      {
+        name: "Blocks",
+      },
+      {
+        name: "Onde encontrar templates free",
+      },
+    ],
+  },
+  {
+    title: "Ferramentas",
+    sections: [
+      {
+        name: "Cores",
+      },
+      {
+        name: "Geradores de código",
+      },
+      {
+        name: "Geradores de background",
+      },
+      {
+        name: "Geradores de botões",
+      },
+      {
+        name: "Geradores de sombras",
+      },
+      {
+        name: "Geradores de animações",
+      },
+    ],
+  },
+  {
+    title: "Otimização",
+    sections: [
+      {
+        name: "Como reutilizar classes",
+      },
+    ],
+  },
+  {
+    title: "Desafio final",
+    sections: [
+      {
+        name: "Do zero ao deploy em 15 minutos",
+      },
+    ],
+  },
+];
+
+export default topics;
